8bf1417e05e1d54319d81d227ddb7eafff8d3bd6
Carlos Fortin <cafortinl@hotmail.com>
2020-03-13 15:23:56 -0600

    Cuarto commit

---
+++ Complejo.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+class Complejo{
+    private:
+        int real, imag;
+    public:
+        Complejo();
+        Complejo(int r, int i);
+        int getReal();
+        int getImg();
+        Complejo operator+ (Complejo operando){
+            return suma(*this, operando);
+        }
+        Complejo operator* (Complejo operando){
+            return mult(*this, operando);
+        }
+        Complejo suma(Complejo r1, Complejo r2);
+        Complejo mult(Complejo r1, Complejo r2);
+};
--- main.cpp
+++ main.cpp
@@ -1,6 +1,97 @@
 #include <iostream>
+#include <vector>
+#include "Vector.hpp"
+#include "Complejo.hpp"
+#include "Racional.hpp"
 
 using namespace std;
+
+vector<Vector<Racional>> racionales;
+vector<Vector<Complejo>> complejos;
+
+void opciones2(int x){
+    switch(x){
+        case 1:{
+            int nX,dX,nY,dY,nZ,dZ;
+            cout << "Ingrese el numerador del elemento x: ";
+            cin >> nX;
+            cout << "Ingrese el denominador del elemento x: ";
+            cin >> dX;
+            cout << "Ingrese el numerador del elemento y: ";
+            cin >> nY;
+            cout << "Ingrese el denominador del elemento y: ";
+            cin >> dY;
+            cout << "Ingrese el numerador del elemento z: ";
+            cin >> nZ;
+            cout << "Ingrese el denominador del elemento z: ";
+            cin >> dZ;
+            Racional x(nX,dX);
+            Racional y(nY,dY);
+            Racional z(nZ,dZ);
+            Vector<Racional> temp(x,y,z);
+            racionales.push_back(temp);
+            break;
+        }
+
+        case 2:{
+            int rX,iX,rY,iY,rZ,iZ;
+            cout << "Ingrese el elemento real del elemento x: ";
+            cin >> rX;
+            cout << "Ingrese el elemento imaginario del elemento x: ";
+            cin >> iX;
+            cout << "Ingrese el elemento real del elemento y: ";
+            cin >> rY;
+            cout << "Ingrese el elemento imaginario del elemento y: ";
+            cin >> iY;
+            cout << "Ingrese el elemento real del elemento z: ";
+            cin >> rZ;
+            cout << "Ingrese el elemento imaginario del elemento z: ";
+            cin >> iZ;
+            Complejo x(rX,iX);
+            Complejo y(rY,iY);
+            Complejo z(rZ,iZ);
+            Vector<Complejo> temp(x,y,z);
+            complejos.push_back(temp);
+            break;
+        }
+
+        default:{
+            cout << "La opcion ingresada no es valida" << endl;
+            break;
+        }
+    }
+}
+
+void opciones1(int x){
+    switch(x){
+        case 1:{
+            int opcion;
+            cout << "Ingresar Vector" << endl;
+            cout << "1. Racional" << endl;
+            cout << "2. Complejo" << endl;
+            cin >> opcion;
+            opciones2(opcion);
+            break;
+        }
+
+        case 2:{
+            break;
+        }
+
+        case 3:{
+            break;
+        }
+
+        case 0:{
+            break;
+        }
+
+        default:{
+            cout << "La opcion ingresada no es valida" << endl;
+            break;
+        }
+    }
+}
 
 void mostrarMenu(){
     cout << "----------Menu----------" << endl;
@@ -15,6 +106,8 @@ int main(){
     do{
         mostrarMenu();
         cin >> opcion;
+        opciones1(opcion);
     }while(opcion != 0);
+    cout << "Fin del programa" << endl;
     return 0;
 }
